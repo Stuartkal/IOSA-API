e4fd5285780a180abd5b42abc6c4abbd8f9ca78a
Stuartkal <Stuartkal@gmail.com>
2021-01-09 20:51:53 +0300

    third deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const mongoose = require('mongoose')
 const cors = require('cors')
-const helmet = require('helmet')
+// const helmet = require('helmet')
 
 require('dotenv').config()
 
@@ -12,9 +12,9 @@ const breedRoutes = require('./routes/breed')
 
 const app = express()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
-app.use(helmet())
+// app.use(helmet())
 
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin','*')
