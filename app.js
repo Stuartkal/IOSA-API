eeb670d07cf98a9bb53d1813af020acceffaab62
Stuartkal <Stuartkal@gmail.com>
2021-01-10 19:55:50 +0300

    cors config3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const mongoose = require('mongoose')
 const cors = require('cors')
-// const helmet = require('helmet')
+
 
 require('dotenv').config()
 
@@ -12,19 +12,26 @@ const breedRoutes = require('./routes/breed')
 
 const app = express()
 
-app.use(cors())
+const corsConfig = {
+    origin: ["http://testing.local", "https://my-movie-db-roberto.herokuapp.com", "http://localhost:3000"],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ['Content-Type']
+}
+
+app.use(cors(corsConfig))
 app.use(express.json())
-// app.use(helmet())
 
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin','*')
-    res.setHeader('Access-Control-Max-Age','*')
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE')
-    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Authorization')
-    next()
-})
 
-app.use('/auth',cors(),userRoutes)
+// app.use((req,res,next) => {
+//     res.setHeader('Access-Control-Allow-Origin','*')
+//     res.setHeader('Access-Control-Max-Age','*')
+//     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE')
+//     res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Authorization')
+//     next()
+// })
+
+app.use('/auth',userRoutes)
 app.use('/breeds',breedRoutes)
 
 
