3f980afdea5842cf4d6b9531a412438bae2ca3fa
v1b3m <vibenjamin6@gmail.com>
2021-01-11 17:23:07 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,43 +1,26 @@
-const express = require('express')
+require('dotenv-safe').config()
 
+const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-
-require('dotenv').config()
 
 const userRoutes = require('./routes/user')
 const breedRoutes = require('./routes/breed')
 
-const app = express()
-
-const corsConfig = {
-    origin: ['http://localhost:3000'],
-    credentials: true,
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type'],
+const errorHandler = (error, req, res, next) => {
+    const { message, data, statusCode } = error
+    return res.status(statusCode || 500).json({ message, data })
 }
 
-app.use(cors(corsConfig))
-app.use(express.json())
+const app = express()
 
-// app.use((req,res,next) => {
-//     res.setHeader('Access-Control-Allow-Origin','*')
-//     res.setHeader('Access-Control-Max-Age','*')
-//     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE')
-//     res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Authorization')
-//     next()
-// })
+app.use(cors())
+app.use(express.json())
 
 app.use('/auth', userRoutes)
 app.use('/breeds', breedRoutes)
 
-app.use((error, req, res, next) => {
-    console.log(error)
-    const status = error.statusCode || 500
-    const message = error.message
-    const data = error.data
-    res.status(status).json({ message: message, data: data })
-})
+app.use(errorHandler)
 
 mongoose
     .connect(process.env.MONGO_DB_URL, {
@@ -47,4 +30,5 @@ mongoose
     })
     .then((result) => {
         app.listen(process.env.PORT || 8080)
+        console.log(`App listening on http://localhost:${process.env.PORT}`)
     })
